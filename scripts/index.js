6bc3b968969c33013a4e4d47d8c0e22dbaf1ec3f
Soloman Garcia <soloman.garcia@gmail.com>
2020-04-15 11:32:36 -0600

    add API key

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,6 @@
+const API_KEY = '0bauf4yFe67huZx5Ixoe6gheZZdnJiVgm9Il3xIW'
+const API_URL = `https://api.nasa.gov/insight_weather/?api_key=${API_KEY}&feedtype=json&ver=1.0`
+
 const previousWeatherToggle = document.querySelector('.show-previous-weather');
 
 const previousWeather = document.querySelector('.previous-weather');
